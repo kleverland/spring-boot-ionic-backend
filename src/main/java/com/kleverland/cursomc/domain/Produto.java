4b9f49d67efd9bbdd0331b761a4a9cd4fecd6ee2
Kleverland Formiga <kleverland@gmail.com>
2020-03-03 09:20:35 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/kleverland/cursomc/domain/Produto.java
+++ src/main/java/com/kleverland/cursomc/domain/Produto.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -24,12 +26,13 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
-	private List<Categoria> categorias = new ArrayList();
+	private List<Categoria> categorias = new ArrayList<>();
 
 	public Produto() {
 	}
