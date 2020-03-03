38cab09227d834e2ac5b7d42135dfc3d55212029
Kleverland Formiga <kleverland@gmail.com>
2020-03-03 11:47:55 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/kleverland/cursomc/domain/Cliente.java
+++ src/main/java/com/kleverland/cursomc/domain/Cliente.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.kleverland.cursomc.domain.enums.TipoCliente;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -28,16 +28,30 @@ public class Cliente implements Serializable{
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
-	private List<Endereco> enderecos= new ArrayList<>();
-	
+	private List<Endereco> enderecos = new ArrayList<>();
+
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
-	
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -61,18 +75,6 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public Cliente() {
-	}
-
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
 	}
 
 	public Integer getId() {
@@ -130,7 +132,13 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-	
-	
-}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+}
