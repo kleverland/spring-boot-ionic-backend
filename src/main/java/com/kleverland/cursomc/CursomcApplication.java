6b1b9c06dbbf124ab64d8624d11bba1058850dc7
Kleverland Formiga <kleverland@gmail.com>
2020-03-03 09:47:22 -0300

    Estado e cidade

--- src/main/java/com/kleverland/cursomc/CursomcApplication.java
+++ src/main/java/com/kleverland/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kleverland.cursomc.domain.Categoria;
+import com.kleverland.cursomc.domain.Cidade;
+import com.kleverland.cursomc.domain.Estado;
 import com.kleverland.cursomc.domain.Produto;
 import com.kleverland.cursomc.repositories.CategoriaRepository;
+import com.kleverland.cursomc.repositories.CidadeRepository;
+import com.kleverland.cursomc.repositories.EstadoRepository;
 import com.kleverland.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriarepository;
 	@Autowired
 	private ProdutoRepository produtorepository;
+	@Autowired
+	private EstadoRepository estadorepository;
+	@Autowired
+	private CidadeRepository cidaderepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,6 +50,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
 		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadorepository.saveAll(Arrays.asList(est1,est2));
+		cidaderepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
