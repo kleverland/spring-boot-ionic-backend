e03861f91ddf7eaca04f53f9f1af8215a38868ec
Kleverland Formiga <kleverlandformiga@MacBook-Pro-de-Kleverland.local>
2020-03-11 11:39:31 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/kleverland/cursomc/services/CategoriaService.java
+++ src/main/java/com/kleverland/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kleverland.cursomc.domain.Categoria;
+import com.kleverland.cursomc.dto.CategoriaDTO;
 import com.kleverland.cursomc.repositories.CategoriaRepository;
 import com.kleverland.cursomc.services.exception.DataIntegrityException;
 import com.kleverland.cursomc.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
