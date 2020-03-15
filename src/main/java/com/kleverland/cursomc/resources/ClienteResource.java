feae3d731d5a4c7c200c9c77b71f89c85167a97e
Kleverland Formiga <kleverland@gmail.com>
2020-03-15 08:40:28 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/kleverland/cursomc/resources/ClienteResource.java
+++ src/main/java/com/kleverland/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.kleverland.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,26 +15,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kleverland.cursomc.domain.Cliente;
 import com.kleverland.cursomc.dto.ClienteDTO;
+import com.kleverland.cursomc.dto.ClienteNewDTO;
 import com.kleverland.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
-		
+
 		return ResponseEntity.ok().body(obj);
-		
+
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
@@ -50,12 +60,11 @@ public class ClienteResource {
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-			@RequestParam(value="page",defaultValue = "0") Integer page, 
-			@RequestParam(value="linesPerPage",defaultValue = "24") Integer linesPerPage, 
-			@RequestParam(value="orderBy",defaultValue = "nome") String orderBy, 
-			@RequestParam(value="direction",defaultValue = "ASC") String direction) {
-		Page<Cliente> list = service.findPage(page, linesPerPage,orderBy,direction);
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 
 		return ResponseEntity.ok().body(listDto);
@@ -71,5 +80,4 @@ public class ClienteResource {
 
 	}
 
-	
 }
--- src/main/java/com/kleverland/cursomc/services/ClienteService.java
+++ src/main/java/com/kleverland/cursomc/services/ClienteService.java
@@ -3,16 +3,25 @@ package com.kleverland.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.kleverland.cursomc.domain.Cidade;
 import com.kleverland.cursomc.domain.Cliente;
+import com.kleverland.cursomc.domain.Endereco;
+import com.kleverland.cursomc.domain.enums.TipoCliente;
 import com.kleverland.cursomc.dto.ClienteDTO;
+import com.kleverland.cursomc.dto.ClienteNewDTO;
+import com.kleverland.cursomc.repositories.CidadeRepository;
 import com.kleverland.cursomc.repositories.ClienteRepository;
+import com.kleverland.cursomc.repositories.EnderecoRepository;
 import com.kleverland.cursomc.services.exception.DataIntegrityException;
 import com.kleverland.cursomc.services.exception.ObjectNotFoundException;
 
@@ -22,10 +31,24 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -54,12 +77,32 @@ public class ClienteService {
 	}
 
 	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null,null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+
+	public Cliente fromDTO(@Valid ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
 	}
 
 }
