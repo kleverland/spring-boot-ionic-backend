133047cb1f4add880332864e0903007e63a4b5cd
Kleverland Formiga <kleverland@gmail.com>
2020-04-07 12:28:32 -0300

    Enviando imagem via endpoint

--- src/main/java/com/kleverland/cursomc/services/ClienteService.java
+++ src/main/java/com/kleverland/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.kleverland.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kleverland.cursomc.domain.Cidade;
 import com.kleverland.cursomc.domain.Cliente;
@@ -43,6 +45,9 @@ public class ClienteService {
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -115,6 +120,10 @@ public class ClienteService {
 		}
 
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
