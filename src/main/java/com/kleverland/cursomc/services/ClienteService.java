2c49898210bfc709bad7510ff56db77f552450d6
Kleverland Formiga <kleverland@gmail.com>
2020-04-09 10:50:51 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/kleverland/cursomc/services/ClienteService.java
+++ src/main/java/com/kleverland/cursomc/services/ClienteService.java
@@ -98,6 +98,18 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
+	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Optional<Cliente> obj = repo.findById(user.getId());
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName()));
+	}
+	
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
